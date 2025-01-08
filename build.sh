f98f8e3bcde005d78315e12e65b4055e9419e05b
Eric de Graaf <endegraaf@gmail.com>
2025-01-08 12:35:11 +0100

    some packages added f.e. vscode

--- build.sh
+++ build.sh
@@ -9,23 +9,16 @@ RELEASE="$(rpm -E %fedora)"
 
 
 # vscode rpm
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-# refresh metadata
-rpm-ostree refresh-md
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen filezilla krita kdenlive syncthing obs-studio vlc chromium syncthing jetbrains-mono-fonts-all code bat netcat
+rpm-ostree install screen filezilla krita kdenlive syncthing obs-studio vlc chromium syncthing jetbrains-mono-fonts-all bat netcat
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
-code # or code-insiders
+rpm-ostree install code
 
 
 
