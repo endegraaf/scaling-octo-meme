edd20152e5f54fa7efc755a96efbd697280cbf46
Eric de Graaf <endegraaf@gmail.com>
2025-01-08 13:41:30 +0100

    some packages added f.e. vscode

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ RELEASE="$(rpm -E %fedora)"
 
 
 # vscode rpm
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 
 # this installs a package from fedora repos
