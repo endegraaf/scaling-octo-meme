7b35f9c8c414790082727f2d790ba91b33314cdb
Eric de Graaf <eric.degraaf@ns.nl>
2025-01-08 18:02:48 +0100

    ctx dir

--- build.sh
+++ build.sh
@@ -8,5 +8,9 @@ mkdir -p /var/lib/alternatives
 /ctx/desktop-packages.sh
 
 
+## NOTES:
+# - /var/lib/alternatives is required to prevent failure with some RPM installs
+# - All RUN commands must end with ostree container commit
+#   see: https://coreos.github.io/rpm-ostree/container/#using-ostree-container-commit
 ostree container commit
 
