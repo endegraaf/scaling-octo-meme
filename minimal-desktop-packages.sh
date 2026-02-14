245c0854fb8746670e012b3fadcebbe99cbab806
Eric de Graaf <endegraaf@gmail.com>
2026-02-14 18:19:44 +0100

    add ags as repo for test

--- minimal-desktop-packages.sh
+++ minimal-desktop-packages.sh
@@ -1,20 +1,16 @@
-#!/usr/bin/bash
-# source https://github.com/m2Giles/m2os/blob/main/desktop-packages.sh
+#!/bin/bash
+set -euo pipefail
+#  #> dnf copr enable retrozinndev/ags 
+# 1. Use the native COPR plugin instead of manual curls
+# Syntax: dnf copr enable <user/project> -y
+REPOS=(
+    "retrozinndev/ags"
+)
 
-set -ouex pipefail
-
-COPR_URL="https://copr.fedorainfracloud.org/coprs"
-
-FEDORA_MAJOR_VERSION=$(rpm -E %fedora)
-
-echo "Add COPR repos for F${FEDORA_MAJOR_VERSION}"
-
-# Fonts
-curl --retry 3 -L  \
-    $COPR_URL/atim/ubuntu-fonts/repo/fedora-"${FEDORA_MAJOR_VERSION}"/atim-ubuntu-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo \
-    -o /etc/yum.repos.d/ericdegraaf-"${FEDORA_MAJOR_VERSION}".repo
-
-
+echo "Enabling COPR repositories..."
+for repo in "${REPOS[@]}"; do
+    dnf copr enable "$repo" -y
+done
 # Layered Applications
 LAYERED_PACKAGES=(
     firefox
