d6894d6b53037a679715a261cc26099fd5e13b96
Eric de Graaf <endegraaf@gmail.com>
2026-02-14 16:53:15 +0100

    test curl

--- minimal-desktop-packages.sh
+++ minimal-desktop-packages.sh
@@ -3,6 +3,13 @@
 
 set -ouex pipefail
 
+
+# Fonts
+curl --retry 3 -L  \
+    $COPR_URL/atim/ubuntu-fonts/repo/fedora-"${FEDORA_MAJOR_VERSION}"/atim-ubuntu-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo \ 
+    -o /etc/yum.repos.d/ericdegraaf-"${FEDORA_MAJOR_VERSION}".repo
+
+
 # Layered Applications
 LAYERED_PACKAGES=(
     firefox
